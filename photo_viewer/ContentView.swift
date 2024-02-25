ea4435208d27ceab6d064fde4d3be245de89aa2c
Sirx <bradx2713@gmail.com>
2024-02-25 23:34:24 +0200

    Initial Commit

---
+++ photo_viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  photo_viewer
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ photo_viewer/photo_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  photo_viewerApp.swift
+//  photo_viewer
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/25.
+//
+
+import SwiftUI
+
+@main
+struct photo_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
